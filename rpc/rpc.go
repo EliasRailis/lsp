f2813f9d5d474cc9f128921d237c4191c975aa97
Elias Railis <elias.railis97@gmail.com>
2024-03-30 20:00:55 +0200

    added the decoding method

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,9 +1,16 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
+
+type BaseMessage struct {
+	Method string `json:"method"`
+}
 
 func EncodeMessage(msg any) string {
 	// serialize the msg value to json
@@ -17,4 +24,27 @@ func EncodeMessage(msg any) string {
 	// print out the content length by using the len() function
 	// and also return the content in json format
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (string, []byte, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+
+	if !found {
+		return "", nil, errors.New("Did not find separator")
+	}
+
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	var baseMessage BaseMessage
+
+	if err := json.Unmarshal(content[:contentLength], &baseMessage); err != nil {
+		return "", nil, err
+	}
+
+	return baseMessage.Method, content[:contentLength], nil
 }
